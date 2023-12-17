06680f71979b7a2676848263ae0274ecb912bfa5
Marat-Shainurov <m_shainurov@mail.ru>
2023-12-17 12:52:20 +0400

    updates the ProdPropertiesChangeAct models and the product.xml view

--- custom/products_marking/models/prod_properties_change_act.py
+++ custom/products_marking/models/prod_properties_change_act.py
@@ -11,47 +11,54 @@ class ProdPropertiesChangeAct(models.Model):
     _name = 'prod_properties_change_act'
     _description = 'Product Properties Change Act'
 
-    product = fields.Many2one('products_marking.product', string='Product')
-    quantity = fields.Integer(string='Quantity')
+    product_id = fields.Many2one('products_marking.product', string='Product', required=True, ondelete='restrict')
+    quantity = fields.Integer(string='Quantity', required=True)
     status = fields.Selection(
         selection=PRODUCT_MOVEMENT_STATUS,
         string="Product movement status",
         required=True,
-        help='Product movement status is used for setting the products movement status.',
-        default='purchase',
-        index=True
-    )
-    old_warehouse = fields.Many2one(
+        default='sale',
+        index=True)
+    old_warehouse_id = fields.Many2one(
         'products_marking.warehouse',
         string='Old warehouse',
-        help='Apply for products from warehouse (movement from warehouse)'
-    )
-    new_warehouse = fields.Many2one(
+        help='Warehouse FROM which the products are moved')
+    new_warehouse_id = fields.Many2one(
         'products_marking.warehouse',
         string='New warehouse',
-        help='Assign new warehouse (movement to warehouse)'
-    )
-
-    cost_or_income = fields.Many2many('cost_or_income_type', string='Cost or income')
-    datetime_act = fields.Datetime(
-        string="Act Date",
+        help='Warehouse TO which the products are moved',
+        required=True)
+    cost_or_income_ids = fields.Many2many(
+        'cost_or_income_item',
+        string='Cost or income items',
+        help='Cost or income items for the product',
+        required=False)
+    datetime_act = fields.Date(
+        string="Act creation date",
         required=True,
         help="Creation date of the Act",
-        default=fields.Datetime.now)
-    #
-    # @api.onchange('status')
-    # def _onchange_status(self):
-    #     if self.status == 'purchase':
-    #         self.old_warehouse.required = False
-    #     else:
-    #         self.old_warehouse.required = True
+        default=fields.Date.today())
 
-    def create(self, values):
-        if 'status' in values and values['status'] == 'purchase':
-            values['old_warehouse'] = False
-        return super(ProdPropertiesChangeAct, self).create(values)
+    def apply_act(self, *args, **kwargs):
+        marked_product = self.env['products_marking.marked_product'].search([('product_id', '=', self.product_id.id)])
+        if marked_product.id and self.status != 'purchase':
+            marked_product.write({
+                'last_assigned_warehouse_id': self.new_warehouse_id.id,
+                'last_assigned_status': self.status,
+                'cost_or_income_item_ids': [(4, cost_or_income_id.id) for cost_or_income_id in self.cost_or_income_ids]
+            })
+            return marked_product
+        else:
+            new_marked_product = self.env['products_marking.marked_product'].create({
+                'product_id': self.product_id.id,
+                'last_assigned_warehouse_id': self.new_warehouse_id.id,
+                'last_assigned_status': self.status,
+                'cost_or_income_item_ids': [(4, cost_or_income_id.id) for cost_or_income_id in self.cost_or_income_ids]
+            })
+            return new_marked_product
 
-    def write(self, values):
-        if 'status' in values and values['status'] == 'purchase':
-            values['old_warehouse'] = False
-        return super(ProdPropertiesChangeAct, self).write(values)
+
+# todo
+#  1. Add the MarkedProduct model + ondelete behavior for all the related models
+#  2. And the business logic to MarkedProduct model and the PPC act applying.
+#  3. Styles
